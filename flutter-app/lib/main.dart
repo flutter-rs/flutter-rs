caa7f528c99a9559c3726fdca55137383a478588
Bach <gliheng@foxmail.com>
2019-01-17 19:04:24 +0800

    hide debug banner using debugShowCheckedModeBanner

--- flutter-app/lib/main.dart
+++ flutter-app/lib/main.dart
@@ -16,6 +16,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      // Since flutter tool is unable to generate AOT code for desktop,
+      // our only option is to hide this banner and use JIT
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),

6bb00a63b30e8fed6a89c5306fd4f05ba7aa626b
Bach <gliheng@foxmail.com>
2019-02-15 09:31:46 +0800

    print flutter print to console

--- flutter-app-demo/scripts/run.py
+++ flutter-app-demo/scripts/run.py
@@ -4,6 +4,7 @@ import re
 import subprocess
 import signal
 import sys
+import threading
 from lib import look_for_proj_dir
 
 def signal_handler(signal, frame):
@@ -14,14 +15,35 @@ signal.signal(signal.SIGINT, signal_handler)
 PROJ_DIR = look_for_proj_dir(os.path.abspath(__file__), 'pubspec.yaml')
 RUST_PROJ_DIR = os.path.join(PROJ_DIR, 'rust')
 
+class CargoThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.observatory_port = ''
+        self.observatory_open = threading.Event()
+
+    def run(self):
+        self.proc = subprocess.Popen(['cargo', 'run'],
+            stdout = subprocess.PIPE,
+            cwd = RUST_PROJ_DIR
+        )
+
+        while True:
+            line = self.proc.stdout.readline()
+            if self.proc.poll() is not None:
+                # proc ended
+                return
+            print(line.decode(), end = '')
+            match = re.search(r'http://(.*?):(\d+)/', line.decode())
+            if match:
+                self.observatory_port = match.group(1)
+                self.observatory_open.set()
+
+
 def cargo_run():
-    proc = subprocess.Popen(['cargo', 'run'], stdout = subprocess.PIPE, cwd = RUST_PROJ_DIR)
-    while True:
-        line = proc.stdout.readline()
-        if not line: break
-        match = re.search(r'http://(.*?):(\d+)/', line.decode())
-        if match:
-            return match.group(1)
+    cargo = CargoThread()
+    cargo.start()
+    cargo.observatory_open.wait()
+    return cargo.observatory_port
 
 if __name__ == '__main__':
     print('>>> Building flutter bundle')

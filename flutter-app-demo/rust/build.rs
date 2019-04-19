8d9933a7e985fec330c6fc09f4f6a027850f0432
juju <gliheng@gmail.com>
2019-04-19 09:30:36 +0800

    move flutter-app-demo to another repo

--- flutter-app-demo/rust/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    #[cfg(target_os="windows")] {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon_with_id("./assets/icon.ico", "GLFW_ICON");
-        res.compile().unwrap();
-    }
-}

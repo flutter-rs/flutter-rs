b3306bb43a9e09c0931b0a387adc10ee4b8f8702
David Craven <david@craven.ch>
2020-01-13 20:59:20 +0100

    Add winit link test.

--- flutter-winit/src/lib.rs
+++ flutter-winit/src/lib.rs
@@ -6,3 +6,14 @@ mod handler;
 mod window;
 
 pub use window::FlutterWindow;
+
+#[cfg(test)]
+mod tests {
+    #[allow(unused)]
+    use flutter_engine;
+
+    #[test]
+    fn test_link() {
+        println!("Linking worked");
+    }
+}

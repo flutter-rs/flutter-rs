0218372d7beaa384e54c9e31fa65d6a7ead61bb3
Chandler Newman <chandler2newman@hotmail.co.uk>
2020-04-28 22:42:43 +0100

    Restore flutter-glfw

---
+++ flutter-glfw/src/lib.rs
@@ -0,0 +1,39 @@
+use crate::window::{CreateError, FlutterWindow, WindowArgs};
+use log::error;
+use std::path::PathBuf;
+
+mod draw;
+mod handler;
+pub mod window;
+
+pub fn init() -> Result<FlutterDesktop, glfw::InitError> {
+    glfw::init(Some(glfw::Callback {
+        f: glfw_error_callback,
+        data: (),
+    }))
+    .map(|glfw| FlutterDesktop { glfw })
+}
+
+#[allow(clippy::trivially_copy_pass_by_ref)]
+fn glfw_error_callback(error: glfw::Error, description: String, _: &()) {
+    error!("GLFW error ({}): {}", error, description);
+}
+
+pub struct FlutterDesktop {
+    glfw: glfw::Glfw,
+}
+
+impl FlutterDesktop {
+    pub fn create_window(
+        &mut self,
+        window_args: &WindowArgs,
+        assets_path: PathBuf,
+        arguments: Vec<String>,
+    ) -> Result<FlutterWindow, CreateError> {
+        FlutterWindow::create(&mut self.glfw, window_args, assets_path, arguments)
+    }
+
+    pub fn glfw(&self) -> glfw::Glfw {
+        self.glfw.clone()
+    }
+}
